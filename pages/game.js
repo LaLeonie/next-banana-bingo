85bb183da01b4ae8ec43c33d621c16387339de61
Leonie Zorzi <Leonie.Zorzi@hotmail.com>
2021-10-09 20:48:39 +0100

    adds links to pages

--- pages/game.js
+++ pages/game.js
@@ -1,3 +1,15 @@
+import Link from "next/link";
+
 export default function Game() {
-  return <h1>Game Page</h1>;
+  return (
+    <>
+      <h1>Game Page</h1>
+      <Link href="/tracker">
+        <a>Add Plants</a>
+      </Link>
+      <Link href="/">
+        <a>I am done</a>
+      </Link>
+    </>
+  );
 }
--- pages/tracker.js
+++ pages/tracker.js
@@ -1,3 +1,15 @@
+import Link from "next/link";
+
 export default function Tracker() {
-  return <h1>Tracker Page</h1>;
+  return (
+    <>
+      <h1>Tracker Page</h1>
+      <Link href="/dashboard">
+        <a>Dashboard</a>
+      </Link>
+      <Link href="/">
+        <a>I am done</a>
+      </Link>
+    </>
+  );
 }
