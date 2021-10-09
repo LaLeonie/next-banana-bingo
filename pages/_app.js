17f31ae2db425e9834c9f1650be82884d5da97fe
Leonie Zorzi <Leonie.Zorzi@hotmail.com>
2021-10-09 21:58:36 +0100

    adds styled component theme

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import "../styles/globals.css";
+import GlobalStyle from "../styles/globalStyles";
 import Theme from "../styles/Theme";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Theme>
+      <GlobalStyle />
       <Component {...pageProps} />
     </Theme>
   );
