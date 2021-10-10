5607b83c3b53702c539d4ea3414c424b92c69be2
Leonie Zorzi <Leonie.Zorzi@hotmail.com>
2021-10-10 11:45:36 +0100

    adds navigation bar and buttons

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,13 +1,35 @@
 import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; // Import the FontAwesomeIcon component
-import { faHome, faArrowLeft } from "@fortawesome/free-solid-svg-icons"; // import the icons you need
+import {
+  faHome,
+  faArrowLeft,
+  faQuestion,
+} from "@fortawesome/free-solid-svg-icons"; // import the icons you need
+import router, { useRouter } from "next/router";
 
 export default function Navbar({ children }) {
+  const router = useRouter();
   return (
     <div className="navbar">
       <div>
         <Link href="/" passHref>
-          <FontAwesomeIcon icon={faHome} />
+          <button>
+            <FontAwesomeIcon icon={faHome} />
+          </button>
+        </Link>
+        <button onClick={() => router.back()}>
+          <FontAwesomeIcon icon={faArrowLeft} />
+        </button>
+      </div>
+      <p>Banana Bingo</p>
+      <div>
+        <Link href="/dashboard" passHref>
+          <button>Dashboard</button>
+        </Link>
+        <Link href="/info" passHref>
+          <button>
+            <FontAwesomeIcon icon={faQuestion} />
+          </button>
         </Link>
       </div>
     </div>
