333431506e913d8417745432dd8f62c27616dd40
Leonie Zorzi <Leonie.Zorzi@hotmail.com>
2021-10-12 20:09:37 +0100

    fixes bug in store.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import "../styles/globals.css";
 import GlobalStyle from "../styles/globalStyles";
 import Theme from "../styles/Theme";
 import Layout from "../components/Layout";
+import { wrapper } from "../store/store";
 
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
@@ -18,4 +19,4 @@ function MyApp({ Component, pageProps }) {
   );
 }
 
-export default MyApp;
+export default wrapper.withRedux(MyApp);
--- store/store.js
+++ store/store.js
@@ -1,14 +1,24 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import { createWrapper } from "next-redux-wrapper";
+
 import { userReducer } from "./user";
 import { gameReducer } from "./game";
+
+const middleware = [thunk];
 
 export const rootReducer = combineReducers({
   user: userReducer,
   game: gameReducer,
 });
 
-export const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware())
-);
+const composeEnhancers =
+  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose;
+
+const enhancer = composeEnhancers(applyMiddleware(...middleware));
+
+const makeStore = () => createStore(rootReducer, enhancer);
+
+export const wrapper = createWrapper(makeStore);
