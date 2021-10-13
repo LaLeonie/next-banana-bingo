42af584289a62ff24d17c482553d669a5cb3eacd
Leonie Zorzi <Leonie.Zorzi@hotmail.com>
2021-10-13 18:55:47 +0100

    adds Countdown component

---
+++ components/Countdown.js
@@ -0,0 +1,6 @@
+import styles from "../styles/Game.module.css";
+import { useCountdown } from "../hooks/useCountdown";
+
+export const Timer = () => {
+  return <dialog className="countdown-container"></dialog>;
+};
