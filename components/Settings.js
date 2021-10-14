87d93fa8ff7e649363a13aed14d8ee9f23b51475
Leonie Zorzi <Leonie.Zorzi@hotmail.com>
2021-10-14 08:54:00 +0100

    adds logic to change difficulty level in redux store

--- components/Settings.js
+++ components/Settings.js
@@ -1,9 +1,18 @@
 import styles from "../styles/Home.module.css";
+import { useSelector, useDispatch } from "react-redux";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
 
+import { getDifficulty, changeDifficulty } from "./../store/game";
+
 export const Settings = ({ setShowSettings }) => {
   const difficulties = ["easy", "medium", "hard"];
+  const dispatch = useDispatch();
+  const difficulty = useSelector(getDifficulty);
+
+  const handleButtonClick = (type) => {
+    dispatch(changeDifficulty(type));
+  };
 
   return (
     <div className={styles.modal}>
@@ -15,7 +24,13 @@ export const Settings = ({ setShowSettings }) => {
       <h2>Set The Difficulty Level</h2>
       <div className="button-container--compressed">
         {difficulties.map((type, i) => (
-          <button key={i}>{type}</button>
+          <button
+            className={difficulty === type ? "selected" : ""}
+            onClick={() => handleButtonClick(type)}
+            key={i}
+          >
+            {type}
+          </button>
         ))}
       </div>
     </div>
