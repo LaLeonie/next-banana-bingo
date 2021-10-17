79dfbbf388975cdaf8ea0e7c6bdf6886340c9e68
Leonie Zorzi <Leonie.Zorzi@hotmail.com>
2021-10-17 19:32:14 +0100

    adds message logic to result page

--- pages/result.js
+++ pages/result.js
@@ -1,14 +1,39 @@
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
 import Link from "next/link";
 import Head from "next/head";
 
+import styles from "./../styles/Result.module.css";
+import { getToday } from "../store/user";
+
+const victoryMessage = (
+  <>
+    <h2>What a victory!</h2>
+    <p>You also get a star for every plant you ate today</p>
+  </>
+);
+
+const loseMessage = (
+  <>
+    <h2>Don't worry!</h2>
+    <p>Tomorrow is another day.</p>
+    <p>You still get a star for every plant you ate today.</p>
+  </>
+);
+
 export default function Result() {
+  const dispatch = useDispatch();
+  const { victory, dailyPlants, extraScore } = useSelector(getToday);
+  const newScore = dailyPlants.length;
+
   return (
     <>
       <Head>
         <title>Result</title>
       </Head>
       <div className="main-content">
-        <h2>Result</h2>
+        {victory ? victoryMessage : loseMessage}
       </div>
 
       <div className="main-footer main-footer-centered">
