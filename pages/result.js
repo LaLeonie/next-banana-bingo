c1f432193ff16f2f0e80ffa97d4de199de8df794
Leonie Zorzi <Leonie.Zorzi@hotmail.com>
2021-10-22 21:35:52 +0100

    adjusts result alert message

--- pages/result.js
+++ pages/result.js
@@ -8,25 +8,28 @@ import styles from "./../styles/Result.module.css";
 import { getToday } from "../store/user";
 import PlantList from "../components/shared/PlantList";
 
-const victoryMessage = (
-  <>
-    <h2>What a victory!</h2>
-    <p>You also get a star for every plant you ate today</p>
-  </>
-);
-
-const loseMessage = (
-  <>
-    <h2>Don't worry!</h2>
-    <p>Tomorrow is another day.</p>
-    <p>You still get a star for every plant you ate today.</p>
-  </>
-);
-
 export default function Result() {
   const dispatch = useDispatch();
-  const { victory, dailyPlants, extraScore } = useSelector(getToday);
-  const newScore = dailyPlants.length;
+  const { victory, dailyPlants } = useSelector(getToday);
+
+  const victoryMessage = (
+    <>
+      <h2 className={styles.header}>What a victory!</h2>
+      <p>You also get a star for every plant you ate today</p>
+    </>
+  );
+
+  const loseMessage = (
+    <>
+      <h2 className={styles.header}>Don't worry!</h2>
+      <div>Tomorrow you have another chance to win Banana Bingo.</div>
+      <div>
+        {dailyPlants.length > 0
+          ? "You still get a star for every plant you ate today."
+          : "But make sure to eat more plants tomorrow."}
+      </div>
+    </>
+  );
 
   return (
     <>
@@ -35,7 +38,11 @@ export default function Result() {
       </Head>
       <div className="main-content">
         {victory ? victoryMessage : loseMessage}
-        <PlantList plants={dailyPlants} />
+
+        {dailyPlants.length > 0 && <PlantList plants={dailyPlants} />}
+        <div className={styles.question}>
+          Have you eaten any more plants today?
+        </div>
       </div>
 
       <div className="main-footer main-footer-centered">
