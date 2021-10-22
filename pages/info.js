e1eaed000e6c17d799cdfdbab94f96bcd44581d1
Leonie Zorzi <Leonie.Zorzi@hotmail.com>
2021-10-22 21:48:32 +0100

    adds styling to Info Page

--- pages/info.js
+++ pages/info.js
@@ -7,29 +7,29 @@ export default function Info() {
       <Head>
         <title>Info</title>
       </Head>
-      <div className="main-content">
+      <div className="main-content instructions">
         <h2>How To Play?</h2>
-        <p>
+        <div>
           Banana Bingo is as easy as standard Bingo. You win if you strike 5
           plants in a row - horizontally, vertically and diagonally. You may
           only select plants that you've eaten today. So to win, you must have
           had your 5 A Day! Be quick, the timer is running. And beware, you can
           only play once a day!
-        </p>
+        </div>
         <ul>
           <li>For a Bingo, you gain 10 stars</li>
           <li>For every plant you ate, you gain an extra stars</li>
         </ul>
-        <p>
+        <div>
           So the more plants you eat in a day, the higher your score. Don't
           worry if you don't find all your plants first, you can add extra
           plants after the game.
-        </p>
-        <p>
+        </div>
+        <div>
           If the plants are too exotic for you (or not exotic enough), you can
           always change the selection in the settings.
-        </p>
-        <p>Happy Bingo!</p>
+        </div>
+        <div>Happy Bingo!</div>
       </div>
 
       <div className="main-footer main-footer-centered">
